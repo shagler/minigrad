36c09560541078656c7e32ce1050d589583dc70b
Shawn Hagler <haglershawn@gmail.com>
2024-07-06 19:20:56 -0500

    remove requires_grad on unneeded tests

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -10,15 +10,15 @@ class TestTensor(unittest.TestCase):
     np.testing.assert_array_equal(t.data, np.array([1, 2, 3]))
 
   def test_addition(self):
-    x = Tensor([1, 2, 3], requires_grad=True)
-    y = Tensor([4, 5, 6], requires_grad=True)
+    x = Tensor([1, 2, 3])
+    y = Tensor([4, 5, 6])
     z = x + y
     self.assertTrue(isinstance(z, Tensor))
     np.testing.assert_array_equal(z.data, np.array([5, 7, 9]))
 
   def test_multiplication(self):
-    x = Tensor([1, 2, 3], requires_grad=True)
-    y = Tensor([4, 5, 6], requires_grad=True)
+    x = Tensor([1, 2, 3])
+    y = Tensor([4, 5, 6])
     z = x * y
     self.assertTrue(isinstance(z, Tensor))
     np.testing.assert_array_equal(z.data, np.array([4, 10, 18]))
