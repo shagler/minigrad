632a11643b058d21877c13dd247454f64d9ad520
Shawn Hagler <haglershawn@gmail.com>
2024-07-06 20:50:18 -0500

    finish simple version of tensor.py

--- minigrad/tensor.py
+++ minigrad/tensor.py
@@ -22,9 +22,25 @@ class Tensor:
 
     def _backward():
       if self.requires_grad:
-        self.grad = self.grad + out.grad if self.grad is not None else out.grad
+        self.grad = out.grad if self.grad is None else self.grad + out.grad
       if other.requires_grad:
-        other.grad = other.grad + out.grad if other.grad is not None else out.grad
+        other.grad = out.grad if other.grad is None else other.grad + out.grad
+    out._backward = _backward
+
+    return out
+
+  def __sub__(self, other):
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    out = Tensor(self.data - other.data,
+      requires_grad=(self.requires_grad or other.requires_grad),
+      _children=(self, other))
+
+    def _backward():
+      if out.grad is not None:
+        if self.requires_grad:
+          self.grad = out.grad if self.grad is None else self.grad + out.grad
+        if other.requires_grad:
+          other.grad = -out.grad if other.grad is None else other.grad - out.grad
     out._backward = _backward
 
     return out
@@ -37,14 +53,32 @@ class Tensor:
 
     def _backward():
       if self.requires_grad:
-        self.grad = self.grad + other.data * out.grad if self.grad is not None else other.data * out.grad
+        self.grad = other.data * out.grad if self.grad is None else \
+          self.grad + other.data * out.grad
       if other.requires_grad:
-        other.grad = other.grad + self.data * out.grad if other.grad is not None else self.data * out.grad
+        other.grad = self.data * out.grad if other.grad is None else \
+          other.grad + self.data * out.grad
     out._backward = _backward
 
     return out
 
-  # @TODO: __div__ and __sub__
+  def __truediv__(self, other):
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    out = Tensor(self.data / other.data,
+      requires_grad=(self.requires_grad or other.requires_grad),
+      _children=(self, other))
+
+    def _backward():
+      if out.grad is not None:
+        if self.requires_grad:
+          self.grad = out.grad / other.data if self.grad is None else \
+            self.grad + out.grad / other.data
+        if other.requires_grad:
+          grad = -out.grad * self.data / (other.data ** 2)
+          other.grad = grad if other.grad is None else other.grad + grad
+    out._backward = _backward
+
+    return out
 
   def relu(self):
     out_data = np.maximum(0, self.data)
@@ -57,3 +91,23 @@ class Tensor:
         grad = np.where(out_data > 0, out.grad, 0)
         self.grad = self.grad + grad if self.grad is not None else grad
     out._backward = _backward
+
+    return out
+
+  def backward(self):
+    if not self.requires_grad:
+      return
+
+    topo = []
+    visited = set()
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    build_topo(self)
+
+    self.grad = np.ones_like(self.data)
+    for v in reversed(topo):
+      v._backward()
--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -16,12 +16,56 @@ class TestTensor(unittest.TestCase):
     self.assertTrue(isinstance(z, Tensor))
     np.testing.assert_array_equal(z.data, np.array([5, 7, 9]))
 
+  def test_subtraction(self):
+    x = Tensor([1, 2, 3])
+    y = Tensor([4, 5, 6])
+    z = x - y
+    self.assertTrue(isinstance(z, Tensor))
+    np.testing.assert_array_equal(z.data, np.array([-3, -3, -3]))
+
   def test_multiplication(self):
     x = Tensor([1, 2, 3])
     y = Tensor([4, 5, 6])
     z = x * y
     self.assertTrue(isinstance(z, Tensor))
     np.testing.assert_array_equal(z.data, np.array([4, 10, 18]))
+
+  def test_division(self):
+    x = Tensor([2, 4, 6])
+    y = Tensor([1, 2, 3])
+    z = x / y
+    self.assertTrue(isinstance(z, Tensor))
+    np.testing.assert_array_equal(z.data, np.array([2, 2, 2]))
+
+  def test_relu(self):
+    x = Tensor([-1, 0, 1], requires_grad=True)
+    y = x.relu()
+    np.testing.assert_array_equal(y.data, np.array([0, 0, 1]))
+
+  def test_backward_add_mul(self):
+    x = Tensor([1, 2, 3], requires_grad=True)
+    y = Tensor([4, 5, 6], requires_grad=True)
+    z = (x + y) * x
+    z.backward()
+    self.assertIsNotNone(x.grad)
+    self.assertIsNotNone(y.grad)
+    if x.grad is not None and y.grad is not None:
+      np.testing.assert_array_almost_equal(x.grad, np.array([6, 9, 12]), decimal=5)
+      np.testing.assert_array_almost_equal(y.grad, np.array([1, 2, 3]), decimal=5)
+
+  def test_backward_sub_div(self):
+    x = Tensor([1, 2, 3], requires_grad=True)
+    y = Tensor([4, 5, 6], requires_grad=True)
+    z = (x - y) / x
+    z.backward()
+    self.assertIsNotNone(x.grad)
+    self.assertIsNotNone(y.grad)
+    if x.grad is not None and y.grad is not None:
+      np.testing.assert_array_almost_equal(x.grad, np.array([4, 1.25, 0.666667]), decimal=5)
+      np.testing.assert_array_almost_equal(y.grad, np.array([-1, -0.5, -1/3]), decimal=5)
+
+
+
 
 if __name__ == '__main__':
   unittest.main()
